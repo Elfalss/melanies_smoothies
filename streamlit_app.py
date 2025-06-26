ecb6f6af6f86b045ae03c4fdcbc170b3e3da2f8a
Elif Aydin Alsancak <74007163+Elfalss@users.noreply.github.com>
2025-06-26 14:51:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,9 +67,10 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!',icon="✅")
 
+#New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 #Changing the LIST to a STRING
 #In order to convert the list to a string, we need to first create a variable and then make sure Python thinks it contains a string.
